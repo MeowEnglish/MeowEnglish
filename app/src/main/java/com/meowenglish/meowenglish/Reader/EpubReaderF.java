9ca4334cc74a91bd2c212728571a245ca292f9dd
dimaparyshkov <dimon.paryshkov@mail.ru>
2021-04-15 14:10:17 +0700

    Added implementation, but not yet the fact that it works

---
+++ app/src/main/java/com/meowenglish/meowenglish/Reader/EpubReaderF.java
@@ -0,0 +1,40 @@
+package com.meowenglish.meowenglish.Reader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import nl.siegmann.epublib.epub.EpubReader;
+
+import nl.siegmann.epublib.domain.Book;
+
+public class EpubReaderF
+{
+    private EpubReader epubReader;
+
+    public EpubReaderF()
+    {
+        epubReader = new EpubReader();
+    }
+
+    public Book ReadFile(String fileName)
+    {
+        FileInputStream epubInputStream = null;
+        Book book = null;
+        try
+        {
+            epubInputStream = new FileInputStream(new File(fileName));
+
+            book = epubReader.readEpub(epubInputStream);
+
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return book;
+    }
+}
