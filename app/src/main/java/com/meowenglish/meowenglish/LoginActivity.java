f0eb0d98e108a586102a9212b3875aba2c978db2
dimaparyshkov <dimon.paryshkov@mail.ru>
2021-04-28 23:34:28 +0700

    Added authorization

--- app/src/main/java/com/meowenglish/meowenglish/LoginActivity.java
+++ app/src/main/java/com/meowenglish/meowenglish/LoginActivity.java
@@ -1,28 +1,36 @@
 package com.meowenglish.meowenglish;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.FirebaseApp;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity
 {
-    String[] Gender = { "Мужской", "Женский", "Свой"};
+    String[] Gender = { "Мужской", "Женский", "Свой "};
 
     private EditText edLogin, edEmail, edPasswordSignUp, edPassword, edName, edOccupation;
-    private Button edSignIn, edSignUp/*создать акк*/, onClickSignUp /*зарегестрироваться*/;
+    private Button edSignIn, edSignUp/*создать акк*/, onCSignUp /*зарегестрироваться*/;
     private Spinner edGender;
     ArrayAdapter<String> adapter;
+    FirebaseUser User;
+
     private FirebaseAuth mAuth;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
@@ -30,13 +38,29 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
         init();
+        Toast.makeText(this,"Тест", Toast.LENGTH_SHORT);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser User = mAuth.getCurrentUser();
+        User = mAuth.getCurrentUser();
+        if  (User != null)
+        {
+            finishLogin();
+        }
     }
+
+    public void finishLogin()
+    {
+        User = mAuth.getCurrentUser();
+        if (User.isEmailVerified()) {
+            Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }
+    }
+
 
     private void init()
     {
@@ -53,7 +77,7 @@ public class LoginActivity extends AppCompatActivity
         edOccupation = findViewById(R.id.edOccupation);
         edSignIn = findViewById(R.id.edSignIn);
         edSignUp = findViewById(R.id.edSignUp);
-        onClickSignUp = findViewById(R.id.onClickSignUp);
+        onCSignUp = findViewById(R.id.onCSignUp);
         mAuth = FirebaseAuth.getInstance();
         disableRegistInter();
     }
@@ -61,7 +85,28 @@ public class LoginActivity extends AppCompatActivity
     /*Кнопка регистрации*/
     public void onClickSignUp(View view)
     {
-
+        if (!TextUtils.isEmpty(edEmail.getText().toString()) && !TextUtils.isEmpty(edName.getText().toString())
+                && !TextUtils.isEmpty(edPasswordSignUp.getText().toString()) && !TextUtils.isEmpty(edOccupation.getText().toString()))
+        {
+            mAuth.createUserWithEmailAndPassword(edEmail.getText().toString(), edPasswordSignUp.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        Toast.makeText(getApplicationContext(), "Регистрация прошла успешно", Toast.LENGTH_SHORT).show();
+                        SendEmailVerif();
+                        disableRegistInter();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Регестрация не удалась", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this,"Заполните все поля", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /*Кнопка создания аккаунта*/
@@ -73,7 +118,47 @@ public class LoginActivity extends AppCompatActivity
     /*Кнопка войти*/
     public void onClickSignIn(View view)
     {
+        if (!TextUtils.isEmpty(edLogin.getText().toString()) && !TextUtils.isEmpty(edPassword.getText().toString()))
+        {
+            User = mAuth.getCurrentUser();
+            if (User.getEmail().toString() == edLogin.getText().toString()) {
+                if (!User.isEmailVerified())
+                    Toast.makeText(getApplicationContext(), "Подтвердите электронную почту", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                mAuth.signInWithEmailAndPassword(edLogin.getText().toString(), edPassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
+                            finishLogin();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Ошибка входа", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+        else{
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+            }
 
+    }
+
+    private void SendEmailVerif() {
+        if (User != null)
+        {
+            User.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Проверь свой Email, для подтверждения аккаунта", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Ошибка, письмо не отправлено", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+    }
     }
 
     /*Убирает интерфейс регистрации и включает интрефейс входа*/
@@ -88,7 +173,7 @@ public class LoginActivity extends AppCompatActivity
         edOccupation.setVisibility(View.GONE);
         edSignIn.setVisibility(View.VISIBLE);
         edSignUp.setVisibility(View.VISIBLE);
-        onClickSignUp.setVisibility(View.GONE);
+        onCSignUp.setVisibility(View.GONE);
     }
 
     /*Убирает интерфейс входа и включает интрефейс регистрации*/
@@ -103,6 +188,6 @@ public class LoginActivity extends AppCompatActivity
         edOccupation.setVisibility(View.VISIBLE);
         edSignIn.setVisibility(View.GONE);
         edSignUp.setVisibility(View.GONE);
-        onClickSignUp.setVisibility(View.VISIBLE);
+        onCSignUp.setVisibility(View.VISIBLE);
     }
 }
